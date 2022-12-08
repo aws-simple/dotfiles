749d3383ff21e0e7d7713b00d03a1f8372a42170
aws-simple <michael.loginovsky@gmail.com>
2022-12-08 13:12:29 +0300

    move .krew_install.log to $ZDOTDIR

--- 10_krew.zsh
+++ 10_krew.zsh
@@ -13,11 +13,11 @@ if [[ ! -x "$HOME/.krew/bin/kubectl-krew" ]]; then
   curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
   tar zxvf "${KREW}.tar.gz" &&
   ./"${KREW}" install krew
-  ) > "$HOME/.krew_install.log" 2>&1
+  ) > "$ZDOTDIR/.krew_install.log" 2>&1
 
-  $HOME/.krew/bin/kubectl-krew update      >> "$HOME/.krew_install.log" 2>&1
-  $HOME/.krew/bin/kubectl-krew install ctx >> "$HOME/.krew_install.log" 2>&1
-  $HOME/.krew/bin/kubectl-krew install ns  >> "$HOME/.krew_install.log" 2>&1
+  $HOME/.krew/bin/kubectl-krew update      >> "$ZDOTDIR/.krew_install.log" 2>&1
+  $HOME/.krew/bin/kubectl-krew install ctx >> "$ZDOTDIR/.krew_install.log" 2>&1
+  $HOME/.krew/bin/kubectl-krew install ns  >> "$ZDOTDIR/.krew_install.log" 2>&1
 
-  echo "'krew' is installed, see log is in '$HOME/.krew_install.log'"
+  echo "'krew' is installed, see log is in '$ZDOTDIR/.krew_install.log'"
 fi
