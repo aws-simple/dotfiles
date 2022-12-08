6d32a4e30a7ed5bcad11c01f5350b59efba34d1b
aws-simple <michael.loginovsky@gmail.com>
2022-12-08 16:23:48 +0300

    tune docs

--- 90_goenv.zsh
+++ 90_goenv.zsh
@@ -6,7 +6,7 @@ if [[ ! -d $HOME/.goenv ]]; then
   mkdir $HOME/.goenv
   cat <<- _EOF
 	Directory "$HOME/.goenv" has been created.
-	Use the following commands to proceed with 'go' installation
+	Use the following commands to proceed with 'go' installation:
 	- goenv install -l        # List all available versions
 	- goenv install <version> # Install the specific <version>
 	- goenv global <version>  # Sets the global Go version to the specific <version>
