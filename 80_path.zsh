5724b2949180f1fde88ddbc18de8e221bfa627c2
aws-simple <michael.loginovsky@gmail.com>
2023-05-11 22:15:14 +0300

    gnu make

--- 80_path.zsh
+++ 80_path.zsh
@@ -16,6 +16,11 @@ if [[ -x "/usr/local/opt/gnu-sed/libexec/gnubin/sed" ]]; then
   export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
 fi
 
+# gnu make
+if [[ -x "/usr/local/opt/make/libexec/gnubin/make" ]]; then
+  export PATH="/usr/local/opt/make/libexec/gnubin:$PATH"
+fi
+
 # brew curl
 if [[ -x "/usr/local/opt/curl/bin/curl" ]] ; then
   export PATH="/usr/local/opt/curl/bin:$PATH"
