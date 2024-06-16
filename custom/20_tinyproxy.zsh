6ff6236824065b555f66fd2da0f2afcf6a74e8a5
Michael Loginovsky <michael@loginovsky.com>
2024-06-16 15:51:01 +0300

    refactor proxy and zsh func names

---
+++ custom/20_tinyproxy.zsh
@@ -0,0 +1,83 @@
+if (( ! $+commands[tinyproxy] )); then
+  return
+fi
+
+PORT_TINY=11811
+PORT_SSH=12345
+
+if [[ ! -x /usr/local/opt/gnu-sed/libexec/gnubin/sed ]]; then
+  return
+fi
+
+if [[ ! -r /usr/local/etc/tinyproxy/tinyproxy.conf ]]; then
+  return
+fi
+
+if [[ ! -r /usr/local/etc/tinyproxy/configured ]]; then
+
+  # restore if we have backup but want to reconfigure it
+  if [[ -r /usr/local/etc/tinyproxy/tinyproxy.conf.bckp ]]; then
+    cat /usr/local/etc/tinyproxy/tinyproxy.conf.bckp > /usr/local/etc/tinyproxy/tinyproxy.conf
+  fi
+
+  # backup
+  cat /usr/local/etc/tinyproxy/tinyproxy.conf > /usr/local/etc/tinyproxy/tinyproxy.conf.bckp
+
+  # the example content is shown below
+  /usr/local/opt/gnu-sed/libexec/gnubin/sed -i \
+    -e "s/^Port .\+/Port ${PORT_TINY}/" \
+    -e '/^$/d' \
+    -e '/^#/d' \
+    /usr/local/etc/tinyproxy/tinyproxy.conf
+  echo "Upstream socks5 127.0.0.1:${PORT_SSH}" >> /usr/local/etc/tinyproxy/tinyproxy.conf
+
+  # means we have configured the service
+  touch /usr/local/etc/tinyproxy/configured
+fi
+
+# it's possible to use 'brew services info tinyproxy --json' to see whether tinyproxy is running, but this is too slow
+pgrep tinyproxy >& /dev/null
+if [[ $? -ne 0 ]]; then
+  brew services restart tinyproxy
+
+  pgrep tinyproxy >& /dev/null
+  if [[ $? -ne 0 ]]; then
+    return
+  fi
+fi
+
+export http_proxy=http://127.0.0.1:${PORT_TINY}
+export HTTP_PROXY=http://127.0.0.1:${PORT_TINY}
+export https_proxy=http://127.0.0.1:${PORT_TINY}
+export HTTPS_PROXY=http://127.0.0.1:${PORT_TINY}
+export no_proxy=10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,127.0.0.0/8
+export NO_PROXY=10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,127.0.0.0/8
+
+if (( ! $+commands[autossh] )); then
+  return
+fi
+
+grep -q "^Host tunnel" ~/.ssh/config >& /dev/null
+if [[ $? -ne 0 ]]; then
+  autossh -f -M 0 tunnel -xCNT -D ${PORT_SSH} \
+    -o ServerAliveInterval=10 \
+    -o ServerAliveCountMax=1 \
+    -o ExitOnForwardFailure=yes \
+    -o StrictHostKeyChecking=accept-new
+fi
+
+alias ash='f_ash() { if [[ -n $1 ]] ; then test "$1" = "kill" && kill $(ps aux | grep autossh | grep -v grep | awk "{print \$2}") || autossh -f -M 0 $1 -xCNT -D \${PORT_SSH} -o ServerAliveInterval=10 -o ServerAliveCountMax=1 -o ExitOnForwardFailure=yes -o StrictHostKeyChecking=accept-new ; else ps aux | grep autossh | grep -v grep ; fi ; } ; f_ash'
+
+### 'tinyproxy.conf' example content
+#   User nobody
+#   Group nobody
+#   Port ${PORT_TINY}
+#   Timeout 600
+#   DefaultErrorFile "/usr/local/Cellar/tinyproxy/1.11.1/share/tinyproxy/default.html"
+#   StatFile "/usr/local/Cellar/tinyproxy/1.11.1/share/tinyproxy/stats.html"
+#   LogLevel Info
+#   MaxClients 100
+#   Allow 127.0.0.1
+#   Allow ::1
+#   ViaProxyName "tinyproxy"
+#   Upstream socks5 127.0.0.1:${PORT_SSH}
---
+++ custom/80_alias.zsh
@@ -0,0 +1,27 @@
+alias ll='ls -la'
+alias lr='ls -ltr'
+alias la='ls -ltra'
+
+alias d='docker'
+alias g='git'
+alias h='helm'
+alias k='kubectl'
+alias t='terraform'
+alias o='openssl'
+
+if [[ -x "$HOME/.krew/bin/kubectl-krew" ]]; then
+  alias kn='kubectl ns'
+  alias kx='kubectl ctx'
+fi
+
+alias eg='f_eg() { if [[ -n $1 ]] ; then env | grep -i $1 ; else env | sort ; fi ; } ; f_eg'
+alias cz='cd $(z | tail -1 | sed -e "s/^[^ ]* *//")'
+
+alias pass32="dd if=/dev/urandom bs=1 count=64 2>/dev/null | base64 | rev | cut -b 5- | rev | tr '[:upper:]' '[:lower:]' | tr -d '[:special:]' | tr -d '[:punct:]' | head -c32 ; echo"
+alias pass16="dd if=/dev/urandom bs=1 count=64 2>/dev/null | base64 | rev | cut -b 5- | rev | tr '[:upper:]' '[:lower:]' | tr -d '[:special:]' | tr -d '[:punct:]' | head -c16 ; echo"
+
+alias reloadshell="source $ZDOTDIR/.zshrc"
+alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
+
+alias kxl='f() { kubectl config get-contexts --no-headers -o name ; } ; f'
+alias knl='f() { kubectl get ns -o jsonpath='\''{range .items[*].metadata}{.name}{"\n"}{end}'\'' ; } ; f'
